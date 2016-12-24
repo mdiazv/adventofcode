4d6479e67bc2fb002d58972fdcfdca0b6e5aef87
Miguel Diaz <diazvillarroelm@gmail.com>
2016-12-24 15:04:29 -0300

    Add solution to Day 13 part 2

--- 2016/13/Main.hs
+++ 2016/13/Main.hs
@@ -8,22 +8,25 @@ import qualified Data.PQueue.Min as PQ
 main :: IO ()
 main = do
   let isOpenTest' = isOpen 10
-  let isOpenPart1' = isOpen 1358
+  let isOpenReal' = isOpen 1358
   let start = (1,1)
   let endTest = (7,4)
-  let endPart1 = (31,39)
+  let endReal = (31,39)
   let test = dijkstra isOpenTest' endTest start
-  let part1 = dijkstra isOpenPart1' endPart1 start
-  putStrLn . show $ test
-  putStrLn . show $ part1
-  putStrLn . show $ (length test) - 1
-  putStrLn . show $ (length part1) - 1
+  let real = dijkstra isOpenReal' endReal start
+  let under50 = map fst $ filter (\x -> snd x <= 50) $ M.toList $ snd real
 --  drawMaze isOpenTest' 9 6
---  drawMaze isOpenPart1' 40 40
+--  drawMaze isOpenReal' 40 40
+  putStr $ show start ++ " -> " ++ show endTest ++ ": "
+  putStrLn . show . length . tail . fst $ test
+  putStr $ show start ++ " -> " ++ show endReal ++ ": "
+  putStrLn . show . length . tail . fst $ real
+  putStr "Closed <= 50 steps: "
+  putStrLn . show $ length under50
 
 type Dist = Int
 type Pos = (Int, Int)
-type PosSet = M.Map Pos Bool
+type PosSet = M.Map Pos Int
 type PosPQ = PQ.MinQueue QueueEntry
 type QueueEntry = (Dist, Pos, [Pos])
 
@@ -36,17 +39,17 @@ drawMaze isOpen x y =
 render :: (Pos -> Bool) -> Pos -> Char
 render isOpen p = if isOpen p then '.' else '#'
 
-dijkstra :: (Pos -> Bool) -> Pos -> Pos -> [Pos]
+dijkstra :: (Pos -> Bool) -> Pos -> Pos -> ([Pos], PosSet)
 dijkstra isOpen end start = dijkstra' isOpen end (PQ.singleton (0, start, [])) M.empty
 
-dijkstra' :: (Pos -> Bool) -> Pos -> PosPQ -> PosSet -> [Pos]
+dijkstra' :: (Pos -> Bool) -> Pos -> PosPQ -> PosSet -> ([Pos], PosSet)
 dijkstra' isOpen end open closed
-  | start == end            = reverse $ start:path
+  | start == end            = (reverse $ start:path, closed)
   | start `M.member` closed = dijkstra' isOpen end open' closed
   | otherwise               = dijkstra' isOpen end augmented closed'
   where
     ((dist, start, path), open') = PQ.deleteFindMin open
-    closed' = M.insert start True closed
+    closed' = M.insert start dist closed
     successors = filter (`M.notMember` closed) $ filter isOpen $ neighbors start
     augmented = foldr addOrUpdate open' successors
 
